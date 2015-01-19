fb373fd5006d7f1dd7ecacfbad4b7e4c0c8467e2
Moano <eric.grall@moano.net>
2015-01-19 13:48:07 +0100

    .

--- js/services/timelinesServices.js
+++ js/services/timelinesServices.js
@@ -8,10 +8,10 @@ var DispatcherStores = require('../dispatcherStores');
 
 
 //------------------------------------------------
-var _Stores = { 0 : { id:0, date:'2014-12-25', title:'Montessori School', contact:'E.GRALL', content:'', type:'Appointment', thumbnail:'', actived:true },
-				1 : { id:1, date:'2014-12-25', title:'Call  To', contact:'D.BON', content:'fica fica', type:'Call', thumbnail:'img/avatar.jpg', actived:false },
+var _Stores = { 0 : { id:0, date:'2014-12-25', title:'Apppointment School', contact:'R.GRALL', content:'', type:'Appointment', thumbnail:'', actived:true },
+				1 : { id:1, date:'2014-12-25', title:'Call  To', contact:'F.BION', content:'fica fica', type:'Call', thumbnail:'img/avatar.jpg', actived:false },
 				2 : { id:2, date:'2014-12-25', title:'Meeting Angularjs', contact:'Paris', content:'', type:'Meeting', thumbnail:'img/continuous-delivery-graphic.png', actived:false },
-				3 : { id:3, date:'2014-11-25', title:'Vol Grenoble', contact:'Personal Travel', content:'', type:'Travel', thumbnail:'', actived:false },
+				3 : { id:3, date:'2014-11-25', title:'Plane to Grenoble', contact:'Personal Travel', content:'', type:'Travel', thumbnail:'', actived:false },
 				4 : { id:4, date:'2014-11-25', title:'Web application project', contact:'REACT.JS', content:'', type:'Project', thumbnail:'', actived:false }
 			};
  
